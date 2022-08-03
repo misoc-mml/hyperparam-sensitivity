17021ecaa8faff601acff1d670ad4d998785ae65
dmachlanski <damian.machlanski@gmail.com>
2022-08-03 15:17:23 +0100

    R-Scorer fixes.

--- make_rscorer.py
+++ make_rscorer.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
 
             (Y_res, T_res), base_score = scorer.run(X_val_fold, t_val_fold, y_val_fold)
 
-            pd.DataFrame(data={'y_res': Y_res, 't_res': T_res}).to_csv(os.path.join(options.output_path, f'rs_{options.base_model}_iter{i+1}_fold{k+1}.csv'), index=False)
+            pd.DataFrame(data={'y_res': Y_res.flatten(), 't_res': T_res.flatten()}).to_csv(os.path.join(options.output_path, f'rs_{options.base_model}_iter{i+1}_fold{k+1}.csv'), index=False)
 
             base_scores.append([i+1, k+1, base_score])
     

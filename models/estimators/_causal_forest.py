Bugfix to MSE and PEHE calculation.

models/estimators/_causal_forest.py
@@ -54,7 +54,7 @@ class CausalForestEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             score = scorer.score(iter_id, fold_id, cate_hat)
 
             result = [iter_id, fold_id, p_id, score]
@@ -71,7 +71,7 @@ class CausalForestEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             cate_test = plugin.get_cate(iter_id, fold_id)
 
             test_pehe = pehe(cate_test, cate_hat)
@@ -95,7 +95,7 @@ class CausalForestEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             ate_hat = np.mean(cate_hat)
 
             test_pehe = pehe(cate_test, cate_hat)

models/estimators/_slearner.py
@@ -74,7 +74,7 @@ class SEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             score = scorer.score(iter_id, fold_id, cate_hat)
 
             result = [iter_id, fold_id, p_id, score]
@@ -91,7 +91,7 @@ class SEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             cate_test = plugin.get_cate(iter_id, fold_id)
 
             test_pehe = pehe(cate_test, cate_hat)
@@ -114,6 +114,9 @@ class SEvaluator():
         else:
             y_test_scaled = y_test
 
+        if y_test_scaled.ndim == 1:
+            y_test_scaled = y_test_scaled.reshape(-1, 1)
+
         preds_filename_base = f'{self.model_name}_iter{iter_id}'
         if fold_id > 0:
             preds_filename_base += f'_fold{fold_id}'
@@ -124,11 +127,11 @@ class SEvaluator():
             preds_filename = f'{preds_filename_base}_param{p_id}.csv'
             df_preds = pd.read_csv(os.path.join(self.opt.results_path, preds_filename))
 
-            cate_hat = df_preds['cate_hat'].to_numpy()
+            cate_hat = df_preds['cate_hat'].to_numpy().reshape(-1, 1)
             ate_hat = np.mean(cate_hat)
 
             # Scaled MSE (y_hat is scaled).
-            y_hat = df_preds['y_hat'].to_numpy()
+            y_hat = df_preds['y_hat'].to_numpy().reshape(-1, 1)
             test_mse = mse(y_hat, y_test_scaled)
 
             r2 = r2_score(y_test_scaled, y_hat)
@@ -149,3 +152,44 @@ class SEvaluator():
             test_results.append(result)
         
         return pd.DataFrame(test_results, columns=results_cols)
+
+class SDebug():
+    def __init__(self, opt):
+        self.opt = opt
+        self.model = get_regressor(self.opt.base_model)
+        self.params_grid = get_params(self.opt.base_model)
+    
+    def run(self, train, test, iter_id, cate_test):
+        X_tr = train[0]
+        t_tr = train[1].flatten()
+        y_tr = train[2].flatten()
+        Xt_tr = np.concatenate([X_tr, t_tr.reshape(-1, 1)], axis=1)
+        X_test = test[0]
+        t_test = test[1].flatten()
+        y_test = test[2]
+        Xt_test = np.concatenate([X_test, t_test.reshape(-1, 1)], axis=1)
+
+        xt0, xt1 = xt_from_x(X_test)
+
+        results = []
+        for param_id, params in enumerate(ParameterGrid(self.params_grid)):
+            model1 = clone(self.model)
+            model1.set_params(**params)
+
+            model1.fit(Xt_tr, y_tr)
+
+            y_hat = model1.predict(Xt_test).reshape(-1, 1)
+            y0_hat = model1.predict(xt0).reshape(-1, 1)
+            y1_hat = model1.predict(xt1).reshape(-1, 1)
+            
+            cate_hat = y1_hat - y0_hat
+
+            test_mse = mse(y_hat, y_test)
+            r2 = r2_score(y_test, y_hat)
+            test_pehe = pehe(cate_test, cate_hat)
+            test_ate = abs_ate(cate_test, cate_hat)
+
+            results.append([iter_id, param_id+1, test_mse, r2, test_ate, test_pehe])
+
+        cols = ['fold_id', 'param_id', 'mse', 'r2_score', 'ate', 'pehe']
+        return pd.DataFrame(results, columns=cols)

models/estimators/_tlearner.py
@@ -127,7 +127,7 @@ class TEvaluator():
             preds_cate_filename = f'{preds_cate_filename_base}_param{p_id}.csv'
             df_cate = pd.read_csv(os.path.join(self.opt.results_path, preds_cate_filename))
 
-            cate_hat = df_cate['cate_hat'].to_numpy()
+            cate_hat = df_cate['cate_hat'].to_numpy().reshape(-1, 1)
             score = scorer.score(iter_id, fold_id, cate_hat)
 
             result = [iter_id, fold_id, p_id, score]
@@ -144,7 +144,7 @@ class TEvaluator():
             preds_cate_filename = f'{preds_cate_filename_base}_param{p_id}.csv'
             df_cate = pd.read_csv(os.path.join(self.opt.results_path, preds_cate_filename))
 
-            cate_hat = df_cate['cate_hat'].to_numpy()
+            cate_hat = df_cate['cate_hat'].to_numpy().reshape(-1, 1)
             cate_test = plugin.get_cate(iter_id, fold_id)
 
             test_pehe = pehe(cate_test, cate_hat)
@@ -165,8 +165,8 @@ class TEvaluator():
             y_test_scaled = y_test
         
         # Split y_test into y0 and y1
-        y0_test = y_test_scaled[t_test < 1]
-        y1_test = y_test_scaled[t_test > 0]
+        y0_test = y_test_scaled[t_test < 1].reshape(-1, 1)
+        y1_test = y_test_scaled[t_test > 0].reshape(-1, 1)
 
         results_cols = ['iter_id', 'param_id', 'mse_m0', 'mse_m1', 'ate', 'pehe', 'ate_hat', 'r2_score_m0', 'r2_score_m1']
         preds_m0_filename_base = f'{self.opt.estimation_model}_{self.opt.base_model}_m0_iter{iter_id}'
@@ -184,23 +184,23 @@ class TEvaluator():
         for p0_id in self.df_m0_params['id']:
             preds_m0_filename = f'{preds_m0_filename_base}_param{p0_id}.csv'
             df_m0 = pd.read_csv(os.path.join(self.opt.results_path, preds_m0_filename))
-            m0_mse[p0_id] = mse(df_m0['y_hat'].to_numpy(), y0_test)
-            m0_r2[p0_id] = r2_score(y0_test, df_m0['y_hat'].to_numpy())
+            m0_mse[p0_id] = mse(df_m0['y_hat'].to_numpy().reshape(-1, 1), y0_test)
+            m0_r2[p0_id] = r2_score(y0_test, df_m0['y_hat'].to_numpy().reshape(-1, 1))
         
         m1_mse = {}
         m1_r2 = {}
         for p1_id in self.df_m1_params['id']:
             preds_m1_filename = f'{preds_m1_filename_base}_param{p1_id}.csv'
             df_m1 = pd.read_csv(os.path.join(self.opt.results_path, preds_m1_filename))
-            m1_mse[p1_id] = mse(df_m1['y_hat'].to_numpy(), y1_test)
-            m1_r2[p1_id] = r2_score(y1_test, df_m1['y_hat'].to_numpy())
+            m1_mse[p1_id] = mse(df_m1['y_hat'].to_numpy().reshape(-1, 1), y1_test)
+            m1_r2[p1_id] = r2_score(y1_test, df_m1['y_hat'].to_numpy().reshape(-1, 1))
 
         test_results = []
         for p_id, p0_id, p1_id in zip(self.df_params['id'], self.df_params['m0'], self.df_params['m1']):
             preds_cate_filename = f'{preds_cate_filename_base}_param{p_id}.csv'
             df_cate = pd.read_csv(os.path.join(self.opt.results_path, preds_cate_filename))
 
-            cate_hat = df_cate['cate_hat'].to_numpy()
+            cate_hat = df_cate['cate_hat'].to_numpy().reshape(-1, 1)
             ate_hat = np.mean(cate_hat)
 
             test_pehe = pehe(cate_test, cate_hat)
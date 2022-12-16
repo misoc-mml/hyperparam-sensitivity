3f6070c9eadfa5da89020995d5f90aafffd88fa2
dmachlanski <damian.machlanski@gmail.com>
2022-12-16 14:31:34 +0000

    Analysis - final results.

--- analysis/compare_save.py
+++ analysis/compare_save.py
@@ -6,12 +6,15 @@ import meta_comparers as mcomp
 out_dir = './tables'
 
 meta_models = ['sl', 'tl', 'ipsws', 'drs', 'dmls', 'xl']
-#meta_models = ['sl', 'tl', 'drs', 'dmls', 'xl']
-base_models = ['l1', 'l2', 'dt', 'rf', 'et', 'kr', 'cb', 'lgbm']
+
+#base_models = ['l1', 'l2', 'dt', 'rf', 'et', 'kr', 'cb', 'lgbm']
+base_models = ['l1', 'l2', 'dt', 'rf', 'et', 'kr', 'cb', 'lgbm', 'mlp']
+#base_models = ['mlp']
 #extra_models = [f'{mm}_{bm}' for mm in ['drs', 'dmls', 'xl'] for bm in ['l1', 'l2', 'dt']]
 #drs_models = [f'{mm}_{bm}' for mm in ['drs'] for bm in ['l1', 'l2', 'dt']]
 #ipsw_models = [f'{mm}_{bm}' for mm in ['ipsws'] for bm in ['l1', 'l2', 'dt', 'rf', 'cb', 'lgbm']]
-standalone_cate_models = ['cf', 'sl_mlp', 'tl_mlp']
+#standalone_cate_models = ['cf', 'sl_mlp', 'tl_mlp']
+standalone_cate_models = []
 #standalone_cate_models = ['cf', 'sl_mlp', 'tl_mlp'] + drs_models
 #all_cate_models = [f'{mm}_{bm}' for mm in meta_models for bm in base_models] + ipsw_models + standalone_cate_models
 all_cate_models = [f'{mm}_{bm}' for mm in meta_models for bm in base_models] + standalone_cate_models
@@ -32,7 +35,8 @@ rscore_base_models = ['dt', 'lgbm', 'kr']
 #rscore_base_models = ['lgbm']
 rscore_dir = '../results/scores/'
 
-datasets = ['ihdp', 'jobs']
+#datasets = ['ihdp', 'jobs', 'twins']
+datasets = ['news']
 all_metrics = {'ihdp': ['ate', 'pehe'], 'jobs': ['att', 'policy'], 'news': ['ate', 'pehe'], 'twins': ['ate', 'pehe']}
 
 for ds in datasets:
@@ -45,14 +49,17 @@ for ds in datasets:
     #comp.compare_risks(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_risks.csv'), index=False)
     #comp.compare_correlations(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_correlations.csv'), index=False)
 
-    mcomp.compare_metrics_meta_est(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_meta_est_sem_latex.csv'), index=False)
+    #mcomp.compare_metrics_meta_est(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_meta_est.csv'), index=False)
     #mcomp.compare_risks_meta_est(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_risks_meta_est.csv'), index=False)
     #mcomp.compare_correlations_meta_est(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_correlations_meta_est.csv'), index=False)
+    #mcomp.compare_test_correlations_est(standalone_cate_models, meta_models, base_models, base_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_test_correlations_est.csv'), index=False)
 
-    mcomp.compare_metrics_meta_base(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_meta_base_sem_latex.csv'), index=False)
+    mcomp.compare_metrics_meta_base(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_meta_base.csv'), index=False)
     #mcomp.compare_risks_meta_base(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_risks_meta_base.csv'), index=False)
     #mcomp.compare_correlations_meta_base(standalone_cate_models, meta_models, base_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_correlations_meta_base.csv'), index=False)
+    mcomp.compare_test_correlations_base(meta_models, base_models, base_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_test_correlations_base.csv'), index=False)
 
-    mcomp.compare_metrics_all(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_all_sem_latex.csv'), index=False)
+    #mcomp.compare_metrics_all(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_metrics_all.csv'), index=False)
     #mcomp.compare_risks_all(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_risks_meta_base.csv'), index=False)
-    #mcomp.compare_correlations_all(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_correlations_meta_base.csv'), index=False)
+    #mcomp.compare_correlations_all(all_cate_models, plugin_models, matching_ks, rscore_base_models, base_dir_ds, plugin_dir_ds, matching_dir_ds, rscore_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_correlations_all.csv'), index=False)
+    #mcomp.compare_test_correlations_all(all_cate_models, base_dir_ds, all_metrics[ds]).to_csv(os.path.join(out_dir, f'{ds}_compare_test_correlations_all.csv'), index=False)
